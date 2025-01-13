d588cb3d4347422fd3f8a66abc8c0cafcdec9f92
Mikuláš Šťastný <mikulas.stastny@bringauto.com>
2025-01-13 12:58:06 +0100

    Add unit tests

---
+++ modules/bringauto_repository/default_test.go
@@ -0,0 +1,353 @@
+package bringauto_repository
+
+import (
+	"bringauto/modules/bringauto_package"
+	"bringauto/modules/bringauto_prerequisites"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"testing"
+)
+
+const (
+	RepoName = "repo"
+	ZipExtension = ".zip"
+
+	pack1Name = "pack1"
+	pack2Name = "pack2"
+	pack3Name = "pack3"
+	pack1FileName = pack1Name + "_file"
+	pack2FileName = pack2Name + "_file"
+	pack3FileName = pack3Name + "_file"
+)
+
+var defaultPlatformString bringauto_package.PlatformString
+var pack1 bringauto_package.Package
+var pack2 bringauto_package.Package
+var pack3 bringauto_package.Package
+
+func TestMain(m *testing.M) {
+	stringExplicit := bringauto_package.PlatformStringExplicit {
+		DistroName: "distro",
+		DistroRelease: "1.0",
+		Machine: "machine",
+	}
+
+	defaultPlatformString = bringauto_package.PlatformString{
+		Mode: bringauto_package.ModeExplicit,
+		String: stringExplicit,
+	}
+	setupPackages()
+	m.Run()
+	deletePackages()
+}
+
+func TestDirDoesNotExists(t *testing.T) {
+	repo := GitLFSRepository {
+		GitRepoPath: RepoName,
+	}
+	err := bringauto_prerequisites.Initialize(&repo)
+	if err == nil {
+		t.Fail()
+	}
+}
+
+func TestDirIsNotGitRepo(t *testing.T) {
+	err := os.MkdirAll(RepoName, 0755)
+	if err != nil {
+		t.Fatalf("can't create repo directory - %s", err)
+	}
+
+	repo := GitLFSRepository {
+		GitRepoPath: RepoName,
+	}
+	err = bringauto_prerequisites.Initialize(&repo)
+	if err == nil {
+		t.Fail()
+	}
+
+	err = os.RemoveAll(RepoName)
+	if err != nil {
+		t.Fatalf("can't delete repo directory - %s", err)
+	}
+}
+
+func TestCreatePackagePath(t *testing.T) {
+	repo, err := initGitRepo()
+	if err != nil {
+		t.Fatalf("can't initialize Git repository or struct - %s", err)
+	}
+
+	packPath := repo.CreatePackagePath(pack1)
+	expectedPackPath := filepath.Join(
+		RepoName,
+		pack1.PlatformString.String.DistroName,
+		pack1.PlatformString.String.DistroRelease,
+		pack1.PlatformString.String.Machine,
+		pack1.Name,
+	)
+
+	if packPath != expectedPackPath {
+		t.Fail()
+	}
+
+	err = deleteGitRepo()
+	if err != nil {
+		t.Fatalf("can't delete Git repository - %s", err)
+	}
+}
+
+func TestCopyToRepositoryOnePackage(t *testing.T) {
+	repo, err := initGitRepo()
+	if err != nil {
+		t.Fatalf("can't initialize Git repository or struct - %s", err)
+	}
+
+	err = repo.CopyToRepository(pack1, pack1Name)
+	if err != nil {
+		t.Errorf("CopyToRepository failed - %s", err)
+	}
+
+	packFilePath := filepath.Join(repo.CreatePackagePath(pack1), pack1.GetFullPackageName() + ZipExtension)
+	_, err = os.ReadFile(packFilePath)
+	if os.IsNotExist(err) {
+		t.Fail()
+	}
+
+	err = deleteGitRepo()
+	if err != nil {
+		t.Fatalf("can't delete Git repository - %s", err)
+	}
+}
+
+func TestCopyToRepositoryMultiplePackages(t *testing.T) {
+	repo, err := initGitRepo()
+	if err != nil {
+		t.Fatalf("can't initialize Git repository or struct - %s", err)
+	}
+
+	err = repo.CopyToRepository(pack1, pack2Name)
+	if err != nil {
+		t.Errorf("CopyToRepository failed - %s", err)
+	}
+
+	err = repo.CopyToRepository(pack2, pack2Name)
+	if err != nil {
+		t.Errorf("CopyToRepository failed - %s", err)
+	}
+
+	err = repo.CopyToRepository(pack3, pack3Name)
+	if err != nil {
+		t.Errorf("CopyToRepository failed - %s", err)
+	}
+
+	pack1FilePath := filepath.Join(repo.CreatePackagePath(pack1), pack1.GetFullPackageName() + ZipExtension)
+	_, err = os.ReadFile(pack1FilePath)
+	if os.IsNotExist(err) {
+		t.Fail()
+	}
+
+	pack2FilePath := filepath.Join(repo.CreatePackagePath(pack2), pack2.GetFullPackageName() + ZipExtension)
+	_, err = os.ReadFile(pack2FilePath)
+	if os.IsNotExist(err) {
+		t.Fail()
+	}
+
+	pack3FilePath := filepath.Join(repo.CreatePackagePath(pack3), pack3.GetFullPackageName() + ZipExtension)
+	_, err = os.ReadFile(pack3FilePath)
+	if os.IsNotExist(err) {
+		t.Fail()
+	}
+
+	err = deleteGitRepo()
+	if err != nil {
+		t.Fatalf("can't delete Git repository - %s", err)
+	}
+}
+
+func TestCommitAllChanges(t *testing.T) {
+	repo, err := initGitRepo()
+	if err != nil {
+		t.Fatalf("can't initialize Git repository or struct - %s", err)
+	}
+
+	err = repo.CopyToRepository(pack1, pack1Name)
+	if err != nil {
+		t.Errorf("CopyToRepository failed - %s", err)
+	}
+	repo.CommitAllChanges()
+
+	err = os.Chdir(RepoName)
+	if err != nil {
+		t.Fatal("can't change directory")
+	}
+
+	cmd := exec.Command("git", "status", "-s")
+	stdout, err := cmd.Output()
+	if err != nil {
+		t.Errorf("git status failed - %s", err)
+	}
+	if len(stdout) > 0 {
+		t.Error("git status not empty after CommitAllChanges")
+	}
+
+	cmd = exec.Command("git", "log")
+	_, err = cmd.Output()
+	if err != nil {
+		t.Error("no commit added")
+	}
+
+	err = os.Chdir("../")
+	if err != nil {
+		t.Fatal("can't change directory")
+	}
+
+	err = deleteGitRepo()
+	if err != nil {
+		t.Fatalf("can't delete Git repository - %s", err)
+	}
+}
+
+func TestRestoreAllChanges(t *testing.T) {
+	repo, err := initGitRepo()
+	if err != nil {
+		t.Fatalf("can't initialize Git repository or struct - %s", err)
+	}
+
+	err = repo.CopyToRepository(pack1, pack1Name)
+	if err != nil {
+		t.Errorf("CopyToRepository failed - %s", err)
+	}
+	repo.RestoreAllChanges()
+
+	err = os.Chdir(RepoName)
+	if err != nil {
+		t.Fatal("can't change directory")
+	}
+
+	cmd := exec.Command("git", "status", "-s")
+	stdout, err := cmd.Output()
+	if err != nil {
+		t.Errorf("git status failed - %s", err)
+	}
+	if len(stdout) > 0 {
+		t.Error("git status not empty after CommitAllChanges")
+	}
+
+	cmd = exec.Command("git", "log")
+	_, err = cmd.Output()
+	if err == nil {
+		t.Error("some commit added")
+	}
+
+	err = os.Chdir("../")
+	if err != nil {
+		t.Fatal("can't change directory")
+	}
+
+	err = deleteGitRepo()
+	if err != nil {
+		t.Fatalf("can't delete Git repository - %s", err)
+	}
+}
+
+func initGitRepo() (GitLFSRepository, error) {
+	err := os.MkdirAll(RepoName, 0755)
+	if err != nil {
+		return GitLFSRepository{}, err
+	}
+	err = os.Chdir(RepoName)
+	if err != nil {
+		return GitLFSRepository{}, err
+	}
+
+	cmd := exec.Command("git", "init")
+	_, err = cmd.Output()
+	if err != nil {
+		return GitLFSRepository{}, err
+	}
+
+	err = os.Chdir("../")
+	if err != nil {
+		return GitLFSRepository{}, err
+	}
+
+	repo := GitLFSRepository {
+		GitRepoPath: RepoName,
+	}
+	err = bringauto_prerequisites.Initialize(&repo)
+
+	return repo, err
+}
+
+func deleteGitRepo() error {
+	return os.RemoveAll(RepoName)
+}
+
+func setupPackages() error {
+	os.Mkdir(pack1Name, 0755)
+	os.Mkdir(pack2Name, 0755)
+	os.Mkdir(pack3Name, 0755)
+
+	file1, err := os.Create(filepath.Join(pack1Name, pack1FileName))
+	if err != nil {
+		return err
+	}
+	file2, err := os.Create(filepath.Join(pack2Name, pack2FileName))
+	if err != nil {
+		return err
+	}
+	file3, err := os.Create(filepath.Join(pack3Name, pack3FileName))
+	if err != nil {
+		return err
+	}
+
+	file1.WriteString("file1 content")
+	file2.WriteString("file2 content")
+	file3.WriteString("file3 content")
+
+	pack1 = bringauto_package.Package{
+		Name: "pack1",
+		VersionTag: "1.0",
+		PlatformString: defaultPlatformString,
+		IsDevLib: false,
+		IsLibrary: false,
+		IsDebug: false,
+	}
+
+	pack2 = bringauto_package.Package{
+		Name: "pack1",
+		VersionTag: "1.0",
+		PlatformString: defaultPlatformString,
+		IsDevLib: true,
+		IsLibrary: true,
+		IsDebug: false,
+	}
+
+	pack3 = bringauto_package.Package{
+		Name: "pack1",
+		VersionTag: "1.0",
+		PlatformString: defaultPlatformString,
+		IsDevLib: false,
+		IsLibrary: true,
+		IsDebug: false,
+	}
+
+	return nil
+}
+
+func deletePackages() error {
+	err := os.RemoveAll(pack1Name)
+	if err != nil {
+		return err
+	}
+	err = os.RemoveAll(pack2Name)
+	if err != nil {
+		return err
+	}
+	err = os.RemoveAll(pack3Name)
+	if err != nil {
+		return err
+	}
+	return nil
+}
---
+++ modules/bringauto_sysroot/default_test.go
@@ -0,0 +1,260 @@
+package bringauto_sysroot
+
+import (
+	"bringauto/modules/bringauto_package"
+	"bringauto/modules/bringauto_prerequisites"
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+const (
+	sysrootDir = "test_sysroot"
+
+	pack1Name = "pack1"
+	pack2Name = "pack2"
+	pack3Name = "pack3"
+	pack1FileName = pack1Name + "_file"
+	pack2FileName = pack2Name + "_file"
+	pack3FileName = pack3Name + "_file"
+)
+
+var defaultPlatformString bringauto_package.PlatformString
+var defaultSysroot Sysroot
+
+
+func TestMain(m *testing.M) {
+	stringExplicit := bringauto_package.PlatformStringExplicit {
+		DistroName: "distro",
+		DistroRelease: "1.0",
+		Machine: "machine",
+	}
+
+	defaultPlatformString = bringauto_package.PlatformString{
+		Mode: bringauto_package.ModeExplicit,
+		String: stringExplicit,
+	}
+
+	defaultSysroot = Sysroot {
+		IsDebug: false,
+		PlatformString: &defaultPlatformString,
+	}
+	err := bringauto_prerequisites.Initialize(&defaultSysroot)
+	if err != nil {
+		panic(err)
+	}
+
+	err = setupPackageFiles()
+	if err != nil {
+		panic(fmt.Sprintf("can't setup package files - %s", err))
+	}
+	m.Run()
+	err = deletePackageFiles()
+	if err != nil {
+		panic(fmt.Sprintf("can't delete package files - %s", err))
+	}
+}
+
+func TestInitializePlatformStringNil(t *testing.T) {
+	var sysroot Sysroot
+	err := bringauto_prerequisites.Initialize(&sysroot)
+	if err == nil {
+		t.Fail()
+	}
+}
+
+func TestInitialize(t *testing.T) {
+	sysroot := Sysroot {
+		IsDebug: false,
+		PlatformString: &defaultPlatformString,
+	}
+	err := bringauto_prerequisites.Initialize(&sysroot)
+	if err != nil {
+		t.Fail()
+	}
+}
+
+func TestGetSysrootPath(t *testing.T) {
+	sysrootPath := defaultSysroot.GetSysrootPath()
+
+	workingDir, err := os.Getwd()
+	if err != nil {
+		t.Fatalf("can't get workinǵ dir - %s", err)
+	}
+	testPath := filepath.Join(workingDir, sysrootDirectoryName, defaultSysroot.PlatformString.Serialize())
+
+	if sysrootPath != testPath {
+		t.Fail()
+	}
+}
+
+func TestCreateSysrootDir(t *testing.T) {
+	sysrootPath := defaultSysroot.GetSysrootPath()
+
+	err := os.RemoveAll(sysrootPath)
+	if err != nil {
+		t.Fatalf("can't remove sysroot dir - %s", err)
+	}
+
+	defaultSysroot.CreateSysrootDir()
+
+	_, err = os.ReadFile(sysrootPath)
+	if os.IsNotExist(err) {
+		t.Fail()
+	}
+}
+
+func TestCopyToSysrootOnePackage(t *testing.T) {
+	err := defaultSysroot.CopyToSysroot(pack1Name, pack1Name)
+	if err != nil {
+		t.Errorf("CopyToSysroot failed - %s", err)
+	}
+
+	pack1Path := filepath.Join(defaultSysroot.GetSysrootPath(), pack1FileName)
+	_, err = os.ReadFile(pack1Path)
+	if os.IsNotExist(err) {
+		t.Fail()
+	}
+
+	err = clearSysroot()
+	if err != nil {
+		t.Errorf("can't delete sysroot dir - %s", err)
+	}
+}
+
+func TestCopyToSysrootMultiplePackages(t *testing.T) {
+	err := defaultSysroot.CopyToSysroot(pack1Name, pack1Name)
+	if err != nil {
+		t.Errorf("CopyToSysroot failed - %s", err)
+	}
+
+	err = defaultSysroot.CopyToSysroot(pack2Name, pack2Name)
+	if err != nil {
+		t.Errorf("CopyToSysroot failed - %s", err)
+	}
+
+	err = defaultSysroot.CopyToSysroot(pack3Name, pack3Name)
+	if err != nil {
+		t.Errorf("CopyToSysroot failed - %s", err)
+	}
+
+	pack1Path := filepath.Join(defaultSysroot.GetSysrootPath(), pack1FileName)
+	_, err = os.ReadFile(pack1Path)
+	if os.IsNotExist(err) {
+		t.Fail()
+	}
+
+	pack2Path := filepath.Join(defaultSysroot.GetSysrootPath(), pack2FileName)
+	_, err = os.ReadFile(pack2Path)
+	if os.IsNotExist(err) {
+		t.Fail()
+	}
+
+	pack3Path := filepath.Join(defaultSysroot.GetSysrootPath(), pack3FileName)
+	_, err = os.ReadFile(pack3Path)
+	if os.IsNotExist(err) {
+		t.Fail()
+	}
+
+	err = clearSysroot()
+	if err != nil {
+		t.Errorf("can't delete sysroot dir - %s", err)
+	}
+}
+
+func TestCopyToSysrootOvewriteFiles(t *testing.T) {
+	err := defaultSysroot.CopyToSysroot(pack1Name, pack1Name)
+	if err != nil {
+		t.Errorf("CopyToSysroot failed - %s", err)
+	}
+
+	err = defaultSysroot.CopyToSysroot(pack1Name, pack1Name)
+	if err == nil {
+		t.Error("ovewriting files not detected")
+	}
+
+	err = clearSysroot()
+	if err != nil {
+		t.Errorf("can't delete sysroot dir - %s", err)
+	}
+}
+
+func TestIsPackageInSysroot(t *testing.T) {
+	sysroot := Sysroot {
+		IsDebug: false,
+		PlatformString: &defaultPlatformString,
+	}
+	err := bringauto_prerequisites.Initialize(&defaultSysroot)
+	if err != nil {
+		t.Fatalf("sysroot initialization failed - %s", err)
+	}
+
+	err = sysroot.CopyToSysroot(pack1Name, pack1Name)
+	if err != nil {
+		t.Errorf("CopyToSysroot failed - %s", err)
+	}
+
+	if !sysroot.IsPackageInSysroot(pack1Name) {
+		t.Error("IsPackageInSysroot returned false after copying package to sysroot")
+	}
+
+	if sysroot.IsPackageInSysroot(pack2Name) {
+		t.Error("IsPackageInSysroot returned true for not copied package")
+	}
+
+	if sysroot.IsPackageInSysroot(pack3Name) {
+		t.Error("IsPackageInSysroot returned true for not copied package")
+	}
+
+	err = clearSysroot()
+	if err != nil {
+		t.Errorf("can't delete sysroot dir - %s", err)
+	}
+}
+
+func clearSysroot() error {
+	sysrootPath := defaultSysroot.GetSysrootPath()
+	return os.RemoveAll(filepath.Dir(sysrootPath))
+}
+
+func setupPackageFiles() error {
+	os.Mkdir(pack1Name, 0755)
+	os.Mkdir(pack2Name, 0755)
+	os.Mkdir(pack3Name, 0755)
+
+	file1, err := os.Create(filepath.Join(pack1Name, pack1FileName))
+	if err != nil {
+		return err
+	}
+	file2, err := os.Create(filepath.Join(pack2Name, pack2FileName))
+	if err != nil {
+		return err
+	}
+	file3, err := os.Create(filepath.Join(pack3Name, pack3FileName))
+	if err != nil {
+		return err
+	}
+
+	file1.WriteString("file1 content")
+	file2.WriteString("file2 content")
+	file3.WriteString("file3 content")
+
+	return nil
+}
+
+func deletePackageFiles() error {
+	err := os.RemoveAll(pack1Name)
+	if err != nil {
+		return err
+	}
+	err = os.RemoveAll(pack2Name)
+	if err != nil {
+		return err
+	}
+	err = os.RemoveAll(pack3Name)
+	if err != nil {
+		return err
+	}
+	return nil
+}
